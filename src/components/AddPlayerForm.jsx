c8cefdc9992620373ca766f40c40482d4dc00878
GongYugyeong <yugyeong0917@naver.com>
2020-10-31 17:50:40 +0900

    AddPlayerForm.jsx useState hooks 적용

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -1,28 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
-class AddPlayerForm extends React.Component {
-  state = {
-    value : ''
+function AddPlayerForm(props) {
+  const [value, setValue]= useState('');
+
+  const formRef = React.createRef();
+  const textRef = React.createRef();
+
+  const handleValueChange = (e) => {
+    setValue(e.target.value);
   }
 
-  constructor(props) {
-    super(props);
-    // createRef()는 클래스 안에서만 사용 가능
-    this.formRef = React.createRef();
-    this.textRef = React.createRef();
-  }
-
-  handleValueChange = (e) => {
-    this.setState({value: e.target.value});
-  }
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('handleSubmit');
 
     // text 노드에 접근하는 방법
-    const form = this.formRef.current;
-    const player = this.textRef.current;  // documnet.getElementById(id)와 같다.
+    const form = formRef.current;
+    const player = textRef.current;  // documnet.getElementById(id)와 같다.
     console.log(form.checkValidity());    // 폼내의 모든 입력 validation을 체크
     console.log(player.validity.valid);  // 입력의 9가지 validation을 체크
 
@@ -31,22 +24,20 @@ class AddPlayerForm extends React.Component {
       return;
     }
 
-    this.props.addPlayer(this.state.value);
-    this.setState({value: ''});
+    props.addPlayer(value);
+    setValue('');
   }
 
-  render() {
-    return (
-      // form 태그 noValidate :  html5 정규화 사용하지 않음
-      // input 태그 required : html5 정규화 사용
-      <form ref={this.formRef} className="form" onSubmit={this.handleSubmit} noValidate>
-        <input ref={this.textRef} type="text" className="input" required placeholder="enter a player's name"
-               value={this.state.value}
-               onChange={this.handleValueChange}/>
-        <input type="submit" className="input" value="AddPlayer"/>
-      </form>
-    );
-  }
+  return (
+    // form 태그 noValidate :  html5 정규화 사용하지 않음
+    // input 태그 required : html5 정규화 사용
+    <form ref={formRef} className="form" onSubmit={handleSubmit} noValidate>
+      <input ref={textRef} type="text" className="input" required placeholder="enter a player's name"
+             value={value}
+             onChange={handleValueChange}/>
+      <input type="submit" className="input" value="AddPlayer"/>
+    </form>
+  );
 }
 
 export default AddPlayerForm;
