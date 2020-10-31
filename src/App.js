9da46b908c949050e751cd82f0da62d56cb0d9eb
GongYugyeong <yugyeong0917@naver.com>
2020-10-31 17:40:50 +0900

    App 컴포넌트에 hooks 적용 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import Player from "./components/Player";
@@ -8,76 +8,63 @@ import _ from 'lodash';
 
 let maxId = 4;    // 임시, 실제로는 DB에서 key값 받아서 넣어줌
 
-class App extends React.Component {
-  state = {
-    players: [
-      {name: 'LDK', score: 0, id: 1},
-      {name: 'HONG', score: 0, id: 2},
-      {name: 'KIM', score: 0, id: 3},
-      {name: 'PARK', score: 0, id: 4},
-    ]
+function App(props) {
+  const [players, setPlayers] = useState([
+    {name: 'LDK', score: 0, id: 1},
+    {name: 'HONG', score: 0, id: 2},
+    {name: 'KIM', score: 0, id: 3},
+    {name: 'PARK', score: 0, id: 4},
+  ])
+
+  const handleRemovePlayer = (id) => {
+    // id를 제외한 상태를 업데이트해야 한다.
+    const newPlayers = players.filter((player) => player.id !== id);
+    setPlayers(newPlayers);  // 비동기 실행된다.
   }
 
-  handleRemovePlayer = (id) => {
-    console.log('handleRemovePlayer: ', id);
-    // 로직 작성
-    this.setState((prevState) => {
-      // id를 제외한 상태를 업데이트해야 한다.
-      const players = prevState.players.filter((player) => player.id !== id);
-      return { players };
+  const handleChangeScore = (delta, id) => {
+    const newPlayers = [ ...players];  // 원본 배열을 새로운 배열로 만듬
+
+    /* 새로운 배열을 돌림 */
+    newPlayers.forEach(player => {
+      if(player.id === id){
+        player.score += delta;
+      }
     })
+    // short hand property: key와 value가 같으면 한쪽 생략 ex) players: players
+    setPlayers(newPlayers);
   }
 
-  handleChangeScore = (delta, id) => {
-    console.log('handleChangeScore / ' + delta + ' / id / '+ id);
-    this.setState((prevState) => {
-      const players = [ ...prevState.players];  // 원본 배열을 새로운 배열로 만듬
-
-      /* 새로운 배열을 돌림 */
-      players.forEach(player => {
-        if(player.id === id){
-          player.score += delta;
-        }
-      })
-      return { players }; // short hand property: key와 value가 같으면 한쪽 생략 ex) players: players
-    })
-  }
-
-  handleAddPlayer = (name) => {
-    console.log('handleAddPlayer / '+ name);
-    this.setState(prevState => {
-      const players = [ ...prevState.players]
-      //추가하는 로직
-      players.push({name: name, score: 0, id: ++maxId});
-      return { players };
-    });
+  const handleAddPlayer = (name) => {
+    const newPlayers = [ ...players]
+    //추가하는 로직
+    newPlayers.push({name: name, score: 0, id: ++maxId});
+    setPlayers(newPlayers);
   }
 
   // 가장 높은 score를 리턴
-  getHighScore() {
-    const maxObj = _.maxBy(this.state.players, 'score');
+  const getHighScore = () => {
+    const maxObj = _.maxBy(players, 'score');
     return maxObj.score ? maxObj.score : null;
   }
 
-  render() {
-    return (
-      <div className='scoreboard'>
-        <Header title='My Scoreboard' players={this.state.players}/>
+  return (
+    <div className='scoreboard'>
+      <Header title='My Scoreboard' players={players}/>
 
-        {
-          this.state.players.map(player => (
-            <CustomPlayer name={player.name} key={player.id}
-                    id={player.id}
-                    score={player.score}
-                    isHighScore={player.score === this.getHighScore()}
-                    changeScore={this.handleChangeScore}
-                    removePlayer={this.handleRemovePlayer} />
-          ))
-        }
-        <AddPlayerForm addPlayer={this.handleAddPlayer}></AddPlayerForm>
-      </div>
-    );
-  }
+      {
+        players.map(player => (
+          <CustomPlayer name={player.name} key={player.id}
+                        id={player.id}
+                        score={player.score}
+                        isHighScore={player.score === getHighScore()}
+                        changeScore={handleChangeScore}
+                        removePlayer={handleRemovePlayer} />
+        ))
+      }
+      <AddPlayerForm addPlayer={handleAddPlayer}></AddPlayerForm>
+    </div>
+  );
 }
 
 export default App;
