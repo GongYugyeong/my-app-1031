be44f24567fca147d47a056dfa310d617c5bbc16
GongYugyeong <yugyeong0917@naver.com>
2020-10-31 15:20:32 +0900

    Stopwatch 타이머 로직 구현

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -3,12 +3,20 @@ import React from "react";
 class Stopwatch extends React.Component {
   tickRef;
   state = {
-    isRunning: false
+    isRunning: false,
+    timer: 0
   }
+
+  tick = () => {
+    if (this.state.isRunning){
+      this.setState({timer: this.state.timer + 1})
+    }
+  }
+
   // Dom(return 안에 있는 태그들)이 랜더링된 직후에 호출
   // REST API 호출, 3rd 라이브러리 로딩
   componentDidMount() {
-    this.tickRef = setInterval(() => {}, 1000);
+    this.tickRef = setInterval((this.tick), 1000);
   }
 
   // Dom이 파괴되기 직전에 호출되는 라이프 사이클 메서드
@@ -32,7 +40,7 @@ class Stopwatch extends React.Component {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <span className="stopwatch-time">0</span>
+        <span className="stopwatch-time">{this.state.timer}</span>
         {
           this.getButton()
         }
